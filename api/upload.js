fedd7568da697a0a6a0b6724736f3c9012e41595
wolfpuplabs <167528982+wolfpuplabs@users.noreply.github.com>
2026-06-19 23:36:14 +0700

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
       access: 'public',
       // PENTING: Ganti teks di bawah ini dengan TOKEN ASLI lu dari Vercel Blob!
       // Pastikan tanda kutip duanya (" ") jangan dihapus ya bang.
-      token: "vercel_blob_rw_ZdID2wEx0aggYllp_AL63LRHVtVk30fBN7xjfUokBA1s7cP",
+      token: "vercel_blob_rw_JhKUcWxykmaQt49w_AV0y2cfxmBFShDPiJI4u3DysBBCh4F",
     });
 
     // 3. Kembalikan response sukses beserta URL file 3D-nya
